78c03de2b8091770cce70e69efcac180f4ffe837
ahellaelsady549-hub <ahellaelsady549@gmail.com>
2026-09-11 17:24:54 +0300

    Update auth.tsx

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -59,7 +59,7 @@ function AuthPage() {
           email,
           password,
           options: {
-            emailRedirectTo: window.location.origin,
+            emailRedirectTo: "https://aswanyy-programer.vercel.app/",
             data: { full_name: fullName },
           },
         });
